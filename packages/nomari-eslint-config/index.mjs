f39f41a69f949edfec138415c38cfccb93319586
Naoki Iwasaki <niwasaki@f13g.com>
2026-01-27 03:10:21 +0900

    accept console.assert

--- packages/nomari-eslint-config/index.mjs
+++ packages/nomari-eslint-config/index.mjs
@@ -37,7 +37,7 @@ export default [
                 },
             ],
             // General code quality
-            'no-console': ['warn', { allow: ['warn', 'error'] }],
+            'no-console': ['warn', { allow: ['warn', 'error', 'assert'] }],
             'prefer-const': 'warn',
             'no-var': 'warn',
             'curly': ['error', 'all'],
